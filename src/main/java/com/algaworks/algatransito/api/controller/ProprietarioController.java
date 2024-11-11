a18332e9cdf1b343ac602fbeea28ea4ef2d5510a
Filipe <filipera3@gmail.com>
2024-11-11 09:24:22 -0300

    implementing query with JPQL

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,30 +1,23 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProprietarioController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> list() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setName("Filipe");
-        proprietario1.setEmail("filipe@gmail.com");
-        proprietario1.setTelefone("61 99367-9805");
 
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setName("Gustavo");
-        proprietario2.setEmail("gustavo@gmail.com");
-        proprietario2.setTelefone("61 98537-9949");
-
-        return Arrays.asList(proprietario1, proprietario2);
-
+        return manager.createQuery("from Proprietario", Proprietario.class)
+                .getResultList();
     }
 }
